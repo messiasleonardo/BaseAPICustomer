b47179c87dbbcf4cb340d04fc306cc7e2508c8af
Leonardo Alves <leonardo.alves@eritel.com.br>
2022-06-15 13:14:22 -0300

    att, fixed method to post and get customer in the FakeDataBase

--- BaseAPI/Controllers/CustomerController.cs
+++ BaseAPI/Controllers/CustomerController.cs
@@ -1,6 +1,8 @@
 ﻿using BaseAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Shared.Models;
+using System.Web.Helpers;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,25 +12,80 @@ namespace BaseAPI.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        private readonly APIConfig _config;
+        //private readonly APIConfig _config;
+        private readonly string _path = $"C:\\Users\\Tecnologia7.ERITELTELECOM\\source\\repos\\BaseAPI\\BaseAPI\\FakeData";
+
+        public CustomerController()
+        {
+            //_path = null;
+            //_path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filepath = Path.Combine(_path, "DataBase.json");
+            _path = filepath;
+        }
 
         //Get all customer inside the DataBase
         [HttpGet]
-        public List<Customer> Get()
+        public List<Customer>? Get()
         {
-            return new List<Customer>();
+            string jsonFromFile;
+            using (var reader = new StreamReader(_path))
+            {
+                jsonFromFile = reader.ReadToEnd();
+            }
+            var removeLastChar = jsonFromFile.Remove(jsonFromFile.Length - 1);
+            var fixedJson = $"[{removeLastChar}]";
+            var customerFromJson = JsonConvert.DeserializeObject<List<Customer>>(fixedJson);
+            return customerFromJson;
         }
 
         [HttpGet("Id")]
-        public Customer Get(int id)
+        public Customer? Get(int id)
         {
-            return new Customer();
+            string jsonFromFile;
+            using (var reader = new StreamReader(_path))
+            {
+                jsonFromFile = reader.ReadToEnd();
+            }
+            var removeLastChar = jsonFromFile.Remove(jsonFromFile.Length - 1);
+            var fixedJson = $"[{removeLastChar}]";
+            var customerFromJson = JsonConvert.DeserializeObject<List<Customer>>(fixedJson);
+            var customer = customerFromJson?.FirstOrDefault(x=>x.Id == id);
+
+            return customer;
         }
 
         [HttpPost]
         public bool Post(Customer customer)
         {
+
+            try
+            {
+                var jsonToWrite = JsonConvert.SerializeObject(customer);
+                var result = FixJson(jsonToWrite);
+
+                using (var write = new StreamWriter(_path, append: true))
+                {
+                    write.WriteLine();
+                    write.Write(result);
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
             return true;
+        }
+
+        private string FixJson(string jsonToWrite)
+        {
+            var part = jsonToWrite.Split('{');
+            var fullText = "{" + $"{part[1]}" + "{" + $"{part[2]}"+'}';
+            var removeLastChar = fullText.Remove(fullText.Length - 1);
+
+            var result = $"{removeLastChar},";
+
+            return result;
         }
 
         [HttpDelete("Id")]
@@ -43,6 +100,6 @@ namespace BaseAPI.Controllers
             return true;
         }
 
-        
+
     }
 }
